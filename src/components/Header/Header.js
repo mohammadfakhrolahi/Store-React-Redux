1c1697b1249287fd09ef23ce9e81adc633faf1a5
Mohammad <fakhrolahi@gmail.com>
2023-11-22 14:28:47 +0330

    Added icons to header Nav.Link.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,8 +8,12 @@ const Header = () => {
         <Container>
           <Navbar.Brand href="/">FC Market</Navbar.Brand>
           <Nav>
-            <Nav.Link href="/cart">Cart</Nav.Link>
-            <Nav.Link href="/account">Account</Nav.Link>
+            <Nav.Link href="/cart">
+              <i className="bi bi-bag-fill fs-5"></i>
+            </Nav.Link>
+            <Nav.Link href="/account">
+              <i className="bi bi-person-circle fs-5"></i>
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
