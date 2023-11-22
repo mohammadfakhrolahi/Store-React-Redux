1a5087d2dd27f596cef9a6abf2aac26f553ffcd8
Mohammad <fakhrolahi@gmail.com>
2023-11-22 14:28:11 +0330

    Installed Bootstrap icon.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,12 @@ import { Container, Navbar, Nav } from 'react-bootstrap'
 const Header = () => {
   return (
     <header>
-      <Navbar className='bg-dark' variant='dark'>
+      <Navbar className="bg-dark" variant="dark">
         <Container>
-          <Navbar.Brand href='/'>FC Market</Navbar.Brand>
+          <Navbar.Brand href="/">FC Market</Navbar.Brand>
           <Nav>
-            <Nav.Link href='/cart'>Cart</Nav.Link>
-            <Nav.Link href='/account'>Account</Nav.Link>
+            <Nav.Link href="/cart">Cart</Nav.Link>
+            <Nav.Link href="/account">Account</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
