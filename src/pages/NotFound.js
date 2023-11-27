e9f665a1ade0766ae9a7f0872e8c03e063cb6321
Mohammad <fakhrolahi@gmail.com>
2023-11-27 13:33:33 +0330

    Style
    Centralized NotFound page's texts.

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,8 +3,9 @@ import { Container } from 'react-bootstrap'
 
 const NotFound = () => {
   return (
-    <Container className='min-vh-60'>
-      <h1>Page Not Found!</h1>
+    <Container className='min-vh-60 d-flex flex-column justify-content-center align-items-center'>
+      <h1>404</h1>
+      <h2>Page Not Found!</h2>
     </Container>
   )
 }
